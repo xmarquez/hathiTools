b547d1bb216b30171c982e742880f30737c99a5b
Xavier Marquez <marquez.x@gmail.com>
2022-02-04 22:59:02 +1300

    New get_hathi_page_meta function for page-level metadata

--- R/hathi-ef-tools.R
+++ R/hathi-ef-tools.R
@@ -32,7 +32,7 @@ download_hathi_ef <- function(htid,
 #' the file has not been cached already, it first attempts to download it
 #' directly from the Hathi Trust server. This function uses code authored by Ben
 #' Schmidt, from his Hathidy package
-#' ([https://github.com/bmschmidt/edinburgh/](https://github.com/HumanitiesDataAnalysis/hathidy)).
+#' ([https://github.com/HumanitiesDataAnalysis/hathidy](https://github.com/HumanitiesDataAnalysis/hathidy)).
 #'
 #' @param htid The Hathi Trust id of the item whose extracted features files are
 #'   to be loaded into memory. If it hasn't been downloaded, the function will
@@ -80,31 +80,146 @@ get_hathi_counts <- function(htid,
     dplyr::mutate(htid = htid, .before = dplyr::everything())
 }
 
-#' Reads the metadata of a single downloaded Hathi Trust extracted features file
+#'Reads the metadata of a single downloaded Hathi Trust extracted features file
 #'
-#' Given a single Hathi Trust ID, this function returns a [tibble] with its
-#' metadata information. If the HT EF file corresponding to this ID has not been
-#' downloaded already, it first attempts to download it directly from the Hathi
-#' Trust server. This function uses code authored by Ben Schmidt, from his
-#' Hathidy package
-#' ([https://github.com/bmschmidt/edinburgh/](https://github.com/HumanitiesDataAnalysis/hathidy)).
+#'Given a single Hathi Trust ID, this function returns a [tibble] with its
+#'volume-level metadata information. If the HT EF file corresponding to this ID
+#'has not been downloaded already, it first attempts to download it directly
+#'from the Hathi Trust server. This function uses code authored by Ben Schmidt,
+#'from his Hathidy package
+#'([https://github.com/HumanitiesDataAnalysis/hathidy](https://github.com/HumanitiesDataAnalysis/hathidy)).
 #'
-#' Note that if you want to extract the metadata of more than one Hathi Trust ID
-#' at a time, it is best to simply query the Workset Builder database using
-#' [get_workset_meta]. Using [get_workset_meta] also guarantees you'll get a
-#' rectangular data with HT ids in the rows. It is also possible to get simple
-#' metadata for large numbers of htids by downloading the big hathifile using
-#' [download_hathifile] and then filtering it.
+#'Note that if you want to extract the metadata of more than one Hathi Trust ID
+#'at a time, it is best to simply query the Workset Builder database using
+#'[get_workset_meta], or to download the JSON files for these HTIDs first using
+#'[rsync_from_hathi] and then running [cache_htids_meta]. It is also possible to
+#'get simple metadata for large numbers of htids by downloading the big
+#'hathifile using [download_hathifile] and then filtering it.
 #'
-#' @param htid The Hathi Trust id of the item whose extracted features files are
-#'   to be downloaded.
-#' @param dir The directory where the file is saved. Defaults to
-#'   `getOption("hathiTools.ef.dir")`, which is just "./hathi-ef/" on load. If
-#'   the file does not exist, this function will first attempt to download it.
+#'@param htid The Hathi Trust id of the item whose extracted features files are
+#'  to be downloaded.
+#'@param dir The directory where the file is saved. Defaults to
+#'  `getOption("hathiTools.ef.dir")`, which is just "./hathi-ef/" on load. If
+#'  the file does not exist, this function will first attempt to download it.
 #'
-#' @return a [tibble] with metadata.
-#' @author Ben Schmidt
-#' @export
+#'@return A [tibble] with metadata with the volume-level metadata for the
+#'  corresponding Hathi Trust ID. This [tibble] can contain the following fields
+#'  (taken from
+#'  [https://wiki.htrc.illinois.edu/pages/viewpage.action?pageId=79069329](https://wiki.htrc.illinois.edu/pages/viewpage.action?pageId=79069329);
+#'  if the field is `NULL`, it is not returned, so the metadata can contain
+#'  fewer fields):
+#'
+#'  \describe{
+#'
+#'  \item{schemaVersion}{The schema version for the metadata block of the
+#'  Extracted Features file. A URL linking to the schema.
+#'  "https://schemas.hathitrust.org/Extracted
+#'  Features_Schema_MetadataSubSchema_v_3.0"}
+#'
+#'  \item{id}{The Handle URL for the volume, which will point to the volume in
+#'  the HathiTrust Digital Library. E.g.
+#'  "http://hdl.handle.net/2027/mdp.39015062779023"}
+#'
+#'  \item{type}{Either "Book", "PublicationVolume", or "CreativeWork" depending
+#'  on the value found in the Issuance field of the corresponding Bibframe
+#'  record. When the Bibframe Issuance value is 'mono', the Book type is
+#'  assigned. When the value is 'serl', the PublicationVolume type is assigned.
+#'  In all other cases the CreativeWork type is assigned. The Bibframe Issuance
+#'  is derived from a variety of fields in the MARC record, including Control
+#'  Fields and Physical Description Fields.}
+#'
+#'  \item{dateCreated}{The date on which the metadata portion of the Extracted
+#'  Features file is generated, in YYYYMMDD format}
+#'
+#'  \item{title}{The title of the volume when the type (above) is "Book" or
+#'  "CreativeWork".}
+#'
+#'  \item{alternateTitle}{An alternate title for a bibliographic entity
+#'  described by the Extracted Features file.}
+#'
+#'  \item{enumerationChronology}{Information regarding which volume, issue,
+#'  and/or year the HathiTrust volume was published.}
+#'
+#'  \item{publisher}{Named list column. Information about the publisher of the
+#'  volume described by the Extracted Features file. Includes type, name, and
+#'  id. `type` is either "Organization" or "Person". `id` is a URL identifying
+#'  the publisher, such as a Handle URL, ORCID, etc. `name` is derived from the
+#'  Imprint field in the MARC record.}
+#'
+#'  \item{pubPlace}{Named list column.  Information about where the volume was
+#'  first published. Includes id, type, and name. `type` is taken from the
+#'  Bibframe Instance's provisionActivity's place rdf:about node, which are
+#'  derived from the country codes in the MARC 008 field.}
+#'
+#'  \item{pubDate}{The year in which that edition of the volume was first
+#'  published.}
+#'
+#'  \item{genre}{List column. Information about the volume's genre, as
+#'  determined by the cataloger of the work. Values are derived from the
+#'  Genre/Form Field in the MARC record.}
+#'
+#'  \item{category}{List column. The volume's topic or topics.Derived from the
+#'  Bibframe Work's ClassificationLcc node. Represents the natural language
+#'  label for the Library of Congress Classification (LCC) value based upon the
+#'  Library of Congress's LCC standard documentation.}
+#'
+#'  \item{language}{The cataloger-determined language or languages of the
+#'  volume. Taken from the Bibframe Work's language's identifiedBy's value node,
+#'  which is derived from the Language Code field in the MARC record. This may
+#'  differ from the automatically detected language of any given page in the
+#'  page-level metadata returned by [get_hathi_page_meta]}
+#'
+#'  \item{accessRights}{The copyright status of the volume. Corresponds to
+#'  attributes in HathiTrust's accessRights database. Derived from a
+#'  HathiTrust-local MARC field (974r) that is added to bibliographic records as
+#'  they are processed at HathiTrust.}
+#'
+#'  \item{lastRightsUpdateDate}{The most recent date the volume's copyright
+#'  status was updated.}
+#'
+#'  \item{Contributor}{List column. Contains information regarding the
+#'  author(s), editor(s), or other agents involved in creating the volume.
+#'  Consists of id, type, and name. id is a URL taken from the Bibframe agent
+#'  that links to an authorities database (e.g., VIAF). type is either the
+#'  Person or Organization. Taken from the Bibframe agent type. name is The name
+#'  of the person or organization who created the volume. Taken from the
+#'  Bibframe agent's label. Derived from a variety of fields in the MARC record,
+#'  including Main Entry Fields, Title and Title-Related Fields, and Added Entry
+#'  Fields.}
+#'
+#'  \item{typeOfResource}{The cataloger-determined resource type of the volume
+#'  (e.g., text, image, etc.).}
+#'
+#'  \item{sourceInstitution}{List column. An array containing information about
+#'  the institution that contributed the volume to HathiTrust. Always has a type
+#'  node and a name node. `id` is a URL identifying the source institution.
+#'  `name` is the name of the source institution.}
+#'
+#'  \item{mainEntityOfPage}{List columns. An array of URLs linking to various
+#'  metadata records describing the volume represented by the Extracted Features
+#'  file. The array typically contains 3 URLs that point to the HathiTrust
+#'  Bibliographic API: HathiTrust brief bibliographic record, HathiTrust full
+#'  bibliographic record, and the HathiTrust catalog record.}
+#'
+#'  \item{oclc}{The OCLC number for the volume. An OCLC number is an identifier
+#'  assigned to items as they are cataloged in a library.}
+#'
+#'  \item{lcc}{The Library of Congress Classification number for the volume. An
+#'  LCC number is a type of call number that would be used to locate an item on
+#'  a library shelf.}
+#'
+#'  \item{lccn}{The Library of Congress Control Number for the volume. An LCCN
+#'  is a unique number that is assigned during cataloging.}
+#'
+#'  \item{issn}{The ISSN of the volume (when a journal).}
+#'
+#'  \item{isbn}{The ISBN of the volume (when a book).}
+#'
+#'  }
+#'
+#'@author Ben Schmidt
+#'@author Xavier Marquez
+#'@export
 #'
 #' @examples
 #' \dontrun{
@@ -117,30 +232,129 @@ get_hathi_counts <- function(htid,
 #' }
 get_hathi_meta <- function (htid, dir = getOption("hathiTools.ef.dir")) {
   local_json <- local_loc(htid, suffix = "json.bz2", dir = dir)
-  meta <- value <- NULL
   if(!file.exists(local_json)) {
     download_hathi_ef(htid, dir = dir)
   }
   meta <- load_json(htid, dir = dir)$metadata
 
-  length_zero_cols <- meta %>%
-    purrr::map_lgl(~{length(.) == 0})
-  meta_names <- names(meta)
-  meta_names <- meta_names[ !length_zero_cols ]
-  meta <- meta[ !length_zero_cols]
-
-  flatten_data <- function(x) {
-    tibble::as_tibble_col(x) %>%
-      tidyr::unnest(value) %>%
-      dplyr::mutate(value = as.character(value))
-  }
-
   meta <- meta %>%
+    purrr::compact() %>%
     purrr::map(flatten_data) %>%
-    dplyr::bind_rows(.id = "field")
+    tibble::as_tibble() %>%
+    dplyr::mutate(htid = htid,
+                  .before = dplyr::everything())
 
-  meta$htid <- htid
   meta
+}
+
+#' Extract the page-level metadata of a single Hathi Trust ID
+#'
+#' Given a single Hathi Trust ID, this function returns a [tibble] with its
+#' page-level metadata information. If the HT EF file corresponding to this ID
+#' has not been downloaded already, it first attempts to download it directly
+#' from the Hathi Trust server. This function uses code authored by Ben Schmidt,
+#' from his Hathidy package
+#' ([https://github.com/HumanitiesDataAnalysis/hathidy](https://github.com/HumanitiesDataAnalysis/hathidy)).
+#'
+#' Note that if you want to extract the page-level metadata of more than one
+#' Hathi Trust ID at a time, it is best to download the JSON files for these
+#' HTIDs first using [rsync_from_hathi] and then running [cache_htids_pagemeta].
+#'
+#' @inheritParams get_hathi_meta
+#'
+#' @return A [tibble] with the page-level metadata for the corresponding Hathi
+#'   Trust ID. The page-level metadata contains the following fields (taken from
+#'   [https://wiki.htrc.illinois.edu/pages/viewpage.action?pageId=79069329](https://wiki.htrc.illinois.edu/pages/viewpage.action?pageId=79069329))
+#'
+#'   \describe{
+#'
+#'   \item{seq}{The sequence number of the page in the volume. Corresponds to
+#'   the digital object, so that the first scan in the volume is "00000001",
+#'   which may be the cover, a title page, or something else.}
+#'
+#'   \item{Description}{A hash of the page content used to compute the features
+#'   for the page. Volumes in HathiTrust may be updated to improve scan or OCR
+#'   quality or correct an issue, which would cause the text data to change,
+#'   and, if features are reprocessed, a new hash would result.}
+#'
+#'   \item{calculatedLanguage}{The most probable language of the text on the
+#'   page. Determined algorithmically, and specified by language codes. Will be
+#'   `NA` if no language detected, or if the language was not recognized by the
+#'   algorithm.}
+#'
+#'   \item{tokenCount}{The total number of tokens detected on the page.}
+#'
+#'   \item{lineCount}{The total number of lines of text detected on the page.}
+#'
+#'   \item{emptyLineCount}{The total number of empty lines on the page.}
+#'
+#'   \item{sentenceCount}{The total number of sentences detected on the page.}
+#'
+#'   \item{sectionStats}{A [tibble] column with stats for each of the sections
+#'   of the page ("header", "body", and "footer"). This tibble has the following
+#'   fields:
+#'
+#'   * section: The seciton of the page.
+#'
+#'   * sectiontokenCount: The total number of tokens detected in the section of
+#'   the page.
+#'
+#'   * sectionlineCount: The total number of lines detected in the section of
+#'   the page.
+#'
+#'   * sectionemptyLineCount The total number of empty lines detected in the
+#'   section of the page.
+#'
+#'   * sectionsentenceCount The total number of sentences detected in the
+#'   section of the page.
+#'
+#'   * sectioncapAlphaSeq The longest length of the alphabetical sequence of
+#'   capital characters starting a line. Only available for the "body" section.
+#'
+#'   * beginCharCount A [tibble] column with the first non-White Space
+#'   characters detected on lines in the section (beginChar) and their
+#'   occurrence counts (beginCharCount).
+#'
+#'   * endCharCount An [tibble] column of the last non-White Space characters on
+#'   detected lines in the section (endChar) and their occurrence counts
+#'   (endCharCount). }
+#'
+#'   }
+#'
+#' @author Ben Schmidt
+#' @author Xavier Marquez
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' # Download the 1862 version of "Democracy in America" by Tocqueville and get
+#' # its page-level metadata
+#'
+#' tmp <- tempdir()
+#'
+#' page_meta <- get_hathi_page_meta("mdp.39015001796443", dir = tmp)
+#'
+#' page_meta %>% tidyr::unnest(sectionStats)
+#' page_meta %>% tidyr::unnest(sectionStats) %>% tidyr::unnest(beginCharCount)
+#'
+#' }
+get_hathi_page_meta <- function(htid, dir = getOption("hathiTools.ef.dir")) {
+
+  local_json <- local_loc(htid, suffix = "json.bz2", dir = dir)
+  if(!file.exists(local_json)) {
+    download_hathi_ef(htid, dir = dir)
+  }
+  load_json(htid, dir = dir) %>%
+    parse_page_meta_volume()
+
+
+}
+
+flatten_data <- function(x) {
+  if(length(x) > 1) {
+    x <- list(x)
+  }
+  x
 }
 
 read_cached_ef_file <- function(filename, cache_type) {
@@ -289,6 +503,78 @@ parse_listified_book <- function(listified_version) {
     purrr::flatten() %>%
     dplyr::bind_rows() %>%
     dplyr::mutate(page = as.numeric(page))
+}
+
+parse_page_meta <- function(page) {
+  seq <- NULL
+  to_parse <- names(page)[!names(page) %in% c("body", "header", "footer")]
+  beginCharCount <- page[c("body", "header", "footer")] %>%
+    purrr::compact() %>%
+    purrr::map("beginCharCount") %>%
+    purrr::map_df(~tibble::enframe(.x, name = "beginChar", value = "beginCount") %>%
+                 tidyr::unnest("beginCount"),
+                 .id = "section")
+
+  if(nrow(beginCharCount) == 0) {
+    beginCharCount <- tibble(section = character(0),
+                             beginCharCount = list(tibble(beginChar = character(0),
+                                                           beginCount = numeric(0))))
+  } else {
+    beginCharCount <- beginCharCount %>%
+      dplyr::group_by(section) %>%
+      dplyr::summarise(beginCharCount = list(tibble(beginChar, beginCount)))
+  }
+
+  endCharCount <- page[c("body", "header", "footer")] %>%
+    purrr::compact() %>%
+    purrr::map("endCharCount") %>%
+    purrr::map_df(~tibble::enframe(.x, name = "endChar", value = "endCount") %>%
+                    tidyr::unnest("endCount"),
+                  .id = "section")
+
+  if(nrow(endCharCount) == 0) {
+    endCharCount <- tibble(section = character(0),
+                            endCharCount = list(tibble(endChar = character(0),
+                                                       endCount = numeric(0))))
+  } else {
+    endCharCount <- endCharCount %>%
+      dplyr::group_by(section) %>%
+      dplyr::summarise(endCharCount = list(tibble(endChar, endCount)))
+  }
+
+  sectionStats <- page[c("body", "header", "footer")] %>%
+    purrr::map_df(~dplyr::as_tibble(purrr::compact(.x[c("tokenCount",
+                                                        "lineCount",
+                                                        "emptyLineCount",
+                                                        "sentenceCount",
+                                                        "capAlphaSeq")])),
+                  .id = "section")
+
+  names(sectionStats)[-1] <- paste0("section", names(sectionStats)[-1])
+
+  sectionStats <- sectionStats %>%
+    dplyr::left_join(beginCharCount, by = "section") %>%
+    dplyr::left_join(endCharCount, by = "section")
+
+  page <- page[to_parse] %>%
+    purrr::discard(is.null) %>%
+    tibble::as_tibble_row() %>%
+    dplyr::mutate(seq = as.numeric(seq),
+                  sectionStats = list(sectionStats))
+
+  page
+}
+
+parse_page_meta_volume <- function(listified_version) {
+  page <- NULL
+  res <- listified_version$features$pages %>%
+    purrr::map_df(parse_page_meta)
+
+  res <- res %>%
+    dplyr::mutate(htid = listified_version$htid,
+           .before = dplyr::everything())
+  res
+
 }
 
 download_http <- function(htid, dir) {
