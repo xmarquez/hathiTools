0e741ee2832f0ea3d2d708d6e024cce7bf9b98bc
Xavier Marquez <marquexa@staff.vuw.ac.nz>
2022-08-24 13:55:00 +1200

    New options on load

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,8 @@
     hathiTools.hathifile.dir = "./raw-hathifiles",
     hathiTools.metadata.dir = "./metadata",
     hathiTools.ef.dir = "./hathi-ef",
-    hathiTools.cachetype = "csv.gz"
+    hathiTools.cachetype = "all",
+    hathiTools.cacheformat = "csv.gz"
   )
   toset <- !(names(op.hathiTools) %in% names(op))
   if(any(toset)) options(op.hathiTools[toset])
