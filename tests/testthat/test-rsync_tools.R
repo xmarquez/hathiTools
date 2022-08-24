a3598a189eb2bb434086c150e45da0b9335c9237
Xavier Marquez <marquexa@staff.vuw.ac.nz>
2022-08-24 14:02:34 +1200

    Eliminating caching tests from the rsync tests

--- tests/testthat/test-rsync_tools.R
+++ tests/testthat/test-rsync_tools.R
@@ -10,59 +10,5 @@ test_that("Can rsync", {
   expect_equal(length(files), 10)
 })
 
-test_that("Can cache to csv", {
-  skip_on_cran()
-  skip_on_ci()
-  skip_if_offline()
-  cache_htids(poetry$htid[1:10], dir = dir)
-  files <- fs::dir_ls(path = dir, recurse = TRUE, glob = "*.csv*")
-  expect_equal(length(files), 10)
-  expect_snapshot(get_hathi_counts(poetry$htid[1], dir = dir))
-})
-
-test_that("Can cache to text2vec.csv", {
-  skip_on_cran()
-  skip_on_ci()
-  skip_if_offline()
-  cache_htids(poetry$htid[1:10], dir = dir, cache_type = "text2vec")
-  files <- fs::dir_ls(path = dir, recurse = TRUE, glob = "*.text2vec.csv")
-  expect_equal(length(files), 10)
-  expect_snapshot(get_hathi_counts(poetry$htid[1], dir = dir, cache_type = "text2vec.csv"))
-})
-
-test_that("Can cache to rds", {
-  skip_on_cran()
-  skip_on_ci()
-  skip_if_offline()
-  cache_htids(poetry$htid[1:10], dir = dir, cache_type = "rds")
-  files <- fs::dir_ls(path = dir, recurse = TRUE, glob = "*.rds")
-  expect_equal(length(files), 30)
-  expect_snapshot(get_hathi_counts(poetry$htid[1], dir = dir, cache_type = "rds"))
-})
-
-test_that("Can cache to arrow", {
-  skip_on_cran()
-  skip_on_ci()
-  skip_if_offline()
-  cache_htids(poetry$htid[1:10], dir = dir, cache_type = "feather")
-  files <- fs::dir_ls(path = dir, recurse = TRUE, glob = "*.feather")
-  expect_equal(length(files), 10)
-  expect_snapshot(get_hathi_counts(poetry$htid[1], dir = dir, cache_type = "feather"))
-})
-
-test_that("Can cache workset", {
-  skip_on_cran()
-  skip_on_ci()
-  skip_if_offline()
-  workset <- poetry[ 1:10, ]
-  class(workset) <- c("hathi_workset", class(workset))
-  files <- fs::dir_ls(path = dir, recurse = TRUE, glob = "*.feather")
-  fs::file_delete(files)
-  cache_htids(workset, dir = dir, cache_type = "feather")
-  files <- fs::dir_ls(path = dir, recurse = TRUE, glob = "*.feather")
-  expect_equal(length(files), 10)
-  expect_snapshot(get_hathi_counts(poetry$htid[1], dir = dir, cache_type = "feather"))
-})
-
 unlink(dir, recursive = TRUE, force = TRUE)
 unlink(dir)
