18c62e7c9f8d4d44599a67c77c1eecbefb264c21
Xavier Marquez <marquexa@staff.vuw.ac.nz>
2022-09-03 12:23:08 +1200

    New startup messages

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,6 @@
     hathiTools.hathifile.dir = "./raw-hathifiles",
     hathiTools.metadata.dir = "./metadata",
     hathiTools.ef.dir = "./hathi-ef",
-    hathiTools.cachetype = "all",
     hathiTools.cacheformat = "csv.gz"
   )
   toset <- !(names(op.hathiTools) %in% names(op))
@@ -16,18 +15,27 @@
 
 .onAttach <- function(libname, pkgname) {
   fields <- NULL
-  if(curl::has_internet()) {
-    msg <- .makeMessage("Unable to load available fields for bookworm db - ",
-                        "internet connection may not be available. ",
-                        "options(\"hathiTools.bookworm.fields\") should be NULL.")
-    tryCatch(fields <- suppressMessages(query_bookworm(method = "returnPossibleFields")),
-             error = function(e) packageStartupMessage(msg))
+  if(is.null(getOption("hathiTools.bookworm.fields"))) {
+    if(curl::has_internet()) {
+      msg <- .makeMessage("Unable to load available fields for bookworm db - ",
+                          "internet connection may not be available. ",
+                          "options(\"hathiTools.bookworm.fields\") should be NULL.")
+      tryCatch(fields <- suppressMessages(query_bookworm(method = "returnPossibleFields")),
+               error = function(e) packageStartupMessage(msg))
+    }
+    if(!is.null(fields)) {
+      options(hathiTools.bookworm.fields = fields$name)
+      packageStartupMessage("Available fields for bookworm queries in the Bookworm2021 db:")
+      packageStartupMessage(paste(fields$name, collapse = ", "))
+      packageStartupMessage("Retrieve options via getOption(\"hathiTools.bookworm.fields\")")
+    }
   }
-  if(!is.null(fields)) {
-    options(hathiTools.bookworm.fields = fields$name)
-    packageStartupMessage("Available fields for bookworm queries in the Bookworm2021 db:")
-    packageStartupMessage(paste(fields$name, collapse = ", "))
-    packageStartupMessage("Retrieve options via options(\"hathiTools.bookworm.fields\")")
-  }
-
+  packageStartupMessage("Currently caching Hathi Trust Extracted Features files to ",
+                        getOption("hathiTools.ef.dir"))
+  packageStartupMessage("Change default caching directory by setting ",
+                        "options(hathiTools.ef.dir = $DIR)")
+  packageStartupMessage("Default cache format ",
+                        getOption("hathiTools.cacheformat"))
+  packageStartupMessage("Change default cache format by setting ",
+                        "options(hathiTools.cacheformat = 'new_cache_format')")
 }
